242112fe43446cbe87f3d8821b74b46f3e8ecb04
Hreeth Gupta <hreethgupta2006@gmail.com>
2025-01-25 01:32:47 +0530

    downgraded to tauri v2

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -95,16 +95,6 @@ async fn check_payment_status(qr_code_id: String) -> Result<bool, String> {
 pub fn run() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![create_qr, check_payment_status])
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }

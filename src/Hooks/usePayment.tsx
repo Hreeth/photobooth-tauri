a91bd8d4cc6940f36c663894b906521baed73cba
Hreeth Gupta <hreethgupta2006@gmail.com>
2025-01-24 16:41:04 +0530

    added razorpay payment

--- src/Hooks/usePayment.tsx
+++ src/Hooks/usePayment.tsx
@@ -1,27 +1,66 @@
 import { invoke } from "@tauri-apps/api/core";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
+
+interface QrResponse {
+    id: string,
+    image_url: string,
+    close_by: number
+}
 
 export default function usePayment() {
-    const [qrCodeUrl, setQrCodeUrl] = useState<string | null>(null)
+    const [qrCode, setQrCode] = useState<QrResponse | undefined>(undefined)
     const [loading, setLoading] = useState<boolean>(false)
     const [error, setError] = useState<string | null>(null)
+    const [paid, setPaid] = useState<boolean | null>(null)
 
-    const fetchQrCode = useCallback(async (amount: number, receipt: string) => {
+    const pollingIntervalRef = useRef<NodeJS.Timeout | null>(null)
+
+    const fetchQrCode = useCallback(async (amt: number) => {
         setLoading(true)
         setError(null)
 
         try {
-            const url = await invoke<string>('create_order', { amount, receipt })
-            setQrCodeUrl(url)
-            return url
+            const res = await invoke<QrResponse>('create_qr', { amount: amt, closeBySecs: 180 })
+            setQrCode(res)
+            setPaid(null)
+            pollPaymentStatus(res)
+            return res
         } catch (err) {
             console.error("Error fetching QR Code:", err)
             setError("Failed to fetch QR Code")
-            return null
+            return undefined
         } finally {
             setLoading(false)
         }
     }, [])
 
-    return { qrCodeUrl, loading, error, fetchQrCode }
+    const pollPaymentStatus = useCallback((qr: QrResponse) => {
+        if (pollingIntervalRef.current) clearInterval(pollingIntervalRef.current)
+
+        const interval = setInterval(async () => {
+            try {
+                const currentTime = Math.floor(Date.now() / 1000)
+                if (currentTime >= qr.close_by) {
+                    console.error("QR Code expired")
+                    setPaid(false)
+                    clearInterval(interval)
+                    return
+                }
+
+                const res = await invoke<boolean>("check_payment_status", { qrCodeId: qr.id })
+                if (res) {
+                    console.log("Payment successful")
+                    setPaid(true)
+                    clearInterval(interval)
+                }
+            } catch (err) {
+                console.error("Failed to check payment status:", err)
+                clearInterval(interval)
+            }
+        }, 2000);
+
+        pollingIntervalRef.current = interval
+    }, [])
+
+    return { qrCode, loading, error, fetchQrCode, paid, pollingIntervalRef }
 }
--- src/Pages/Form/Payment/index.tsx
+++ src/Pages/Form/Payment/index.tsx
@@ -1,23 +1,61 @@
 import { motion } from 'framer-motion'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../../../Components/Header'
 import { useNavigate } from 'react-router-dom'
 
-import qr from '../../../assets/Images/sampleQR.png'
-
 import './styles.css'
+import usePayment from '../../../Hooks/usePayment'
+import reset from '../../../Utils/reset'
+import { useData } from '../../../Contexts/DataContext'
+import calculate from '../../../Utils/calculate'
 
 export default function Payment() {
   const navigate = useNavigate()
+  const [time, setTime] = useState<number>(0)
+  const { qrCode, loading, fetchQrCode, paid, pollingIntervalRef } = usePayment()
+
+  const { setOptions, options, plans } = useData()
+  
+  useEffect(() => {
+    fetchQrCode(calculate(options, plans))
+  }, [fetchQrCode])
+  
+  useEffect(() => {
+    switch (paid) {
+      case false: {
+        reset(setOptions, navigate)
+        break
+      }
+      case true: {
+        navigate('/countdown')
+        break
+      }
+      default: {
+        break
+      }
+    }
+
+    console.log(paid)
+  }, [paid])
 
   useEffect(() => {
-    const changeTimeout = setTimeout(() => {
-      navigate('/countdown')
-    }, 5000);
+    if (qrCode) {
+      const timeInterval = setInterval(() => {
+        const currentTime = Math.floor(Date.now() / 1000)
+        const timeLeft = qrCode.close_by - currentTime
 
-    return () => clearTimeout(changeTimeout)
-  }, [])
-  
+        if (timeLeft <= 0) {
+          clearInterval(timeInterval)
+          setTime(0)
+        } else {
+          setTime(timeLeft)
+        }
+      }, 1000);
+
+      return () => clearInterval(timeInterval)
+    }
+  }, [qrCode, setOptions, navigate])
+
   return (
     <motion.div
       id='payment'
@@ -25,19 +63,28 @@ export default function Payment() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-        <Header />
-        <div className='payment-container'>
-          <div className="payment-heading">
-            <div className="payment-title">Scan the QR to make payment</div>
-            <div className="payment-subtitle">QR will expire in 129s</div>
-          </div>
-          <div className="qr-container">
-            <div className="qr-title">Payment</div>
-            <div id="qr">
-              <img src={qr} alt="" />
+        <Header
+          backCallback={() => {
+            if (pollingIntervalRef.current) clearInterval(pollingIntervalRef.current)
+            navigate(-1)
+          }}
+        />
+        {
+          !loading && (
+          <div className='payment-container'>
+            <div className="payment-heading">
+              <div className="payment-title">Scan the QR to make payment</div>
+              <div className="payment-subtitle">QR will expire in {time}s</div>
+            </div>
+            <div className="qr-container">
+              <div className="qr-title">Payment</div>
+              <div id="qr">
+                <img src={qrCode?.image_url} alt="" />
+              </div>
             </div>
           </div>
-        </div>
+          )
+        }
     </motion.div>
   )
 }

8573145c1e56c9047153d27ca71c56ca7d99d57d
Hreeth Gupta <hreethgupta2006@gmail.com>
2025-04-12 17:43:11 +0530

    added proper layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react'
 import AnimatedRoutes from './AnimatedRoutes'
 
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    const controller = new AbortController()
+    window.addEventListener('gesturestart', (e) => e.preventDefault(), { signal: controller.signal });
+    window.addEventListener('gesturechange', (e) => e.preventDefault(), { signal: controller.signal });
+    window.addEventListener('gestureend', (e) => e.preventDefault(), { signal: controller.signal });
+
+    return () => controller.abort()
+  }, [])
+
   return (
     <>
       <div id="main">
